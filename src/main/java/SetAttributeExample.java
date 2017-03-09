4cc628ccd098a7fb092683fc662714282d7f0008
JieNewSky <jiechen@newskysecurity.com>
2017-03-09 14:19:45 -0800

    update

---
+++ src/main/java/SetAttributeExample.java
@@ -0,0 +1,31 @@
+import java.io.FileOutputStream;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+
+public class SetAttributeExample {
+    public static void main(String[] args) {
+        Document document = new Document();
+        try {
+            PdfWriter writer = PdfWriter
+                    .getInstance(
+                            document,
+                            new FileOutputStream(
+                                    "H:\\workspaceGithubEclipse\\itext-examples\\target\\SetAttributeExample.pdf"));
+            document.open();
+            document.add(new Paragraph("Some content here"));
+
+            // Set attributes here
+            document.addAuthor("Lokesh Gupta");
+            document.addCreationDate();
+            document.addCreator("HowToDoInJava.com");
+            document.addTitle("Set Attribute Example");
+            document.addSubject("An example to show how attributes can be added to pdf files.");
+
+            document.close();
+            writer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
